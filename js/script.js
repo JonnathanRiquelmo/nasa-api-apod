e3d1ce70beda8b93a8a3031ec5643f6514ec94ca
JonnathanRiquelmo <jonnathan.riquelmo@gmail.com>
2022-05-24 01:49:39 -0300

    Save Favorites in localStorage()

--- js/script.js
+++ js/script.js
@@ -10,30 +10,31 @@ const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
 function updateDOM() {
     resultsArray.forEach((result) => {
         // Card Container
         const card = document.createElement('div');
         card.classList.add('card');
-        
+
         // Link
         const link = document.createElement('a');
         link.href = result.hdurl;
         link.title = 'View Full Image';
         link.target = '_blank';
-        
+
         // Image
         const image = document.createElement('img');
         image.src = result.url;
         image.alt = 'NASA Picture of the Day';
         image.loading = 'lazy';
         image.classList.add('card-img-top');
-        
+
         // Card Body
         const cardBody = document.createElement('div');
         cardBody.classList.add('card-body');
-        
+
         // Card Title
         const cardTitle = document.createElement('h5');
         card.classList.add('card-title');
@@ -41,8 +42,9 @@ function updateDOM() {
 
         // Save Text
         const saveText = document.createElement('p');
-        saveText.classList.add('clickable');
+        saveText.classList.add('clickableFav');
         saveText.textContent = 'Add To favorites';
+        saveText.setAttribute('onClick', `saveFavorite('${result.url}')`);
 
         // Card Text
         const cardText = document.createElement('p');
@@ -70,12 +72,28 @@ function updateDOM() {
     });
 }
 
+// Add Result to Favorites
+function saveFavorite(itemUrl) {
+    // Loop through results array to select favorite
+    resultsArray.forEach((item) => {
+        if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+            favorites[itemUrl] = item;
+            // Show Save Confirmation for 2 seconds
+            saveConfirmed.hidden = false;
+            setTimeout(() => {
+                saveConfirmed.hidden = true;
+            }, 2000);
+            //Set Favorites in localStorage
+            localStorage.setItem('nasaFavorites', JSON.stringify(favorites));
+        }
+    });
+}
+
 // Get 10 imgs from NASA API
 async function getNasaPictures() {
     try {
         const response = await fetch(apiUrl);
-        resultsArray = await response.json(); 
-        console.log(resultsArray);
+        resultsArray = await response.json();
         updateDOM();
     } catch (error) {
         console.log(error)
